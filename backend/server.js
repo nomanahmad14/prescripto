1d207b17b5634e2a90a7904669f732663d4b4a78
Noman Ahmad <noman22221@gmail.com>
2026-01-22 19:02:24 +0530

    fixed cors

--- backend/server.js
+++ backend/server.js
@@ -16,33 +16,24 @@ connectCloudinary()
 
 //MIDDLEWARES
 
-app.use(express.json());
+import cors from "cors";
+
 const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:5174",
+  "http://localhost:5173", // frontend local
+  "http://localhost:5174", // admin local
   "https://prescripto-frontend-rouge.vercel.app",
-  "https://prescripto-ebon-three.vercel.app"
+  "https://prescripto-ebon-three.vercel.app" // admin vercel
 ];
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin (like Postman)
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("CORS not allowed"));
-      }
-    },
+    origin: allowedOrigins,
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
   })
 );
 
-app.options("*", cors());
 
 //API End Points
 
