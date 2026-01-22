7fd89e0a6cd89cda116a2e2336e8602cad8852ee
Noman Ahmad <noman22221@gmail.com>
2026-01-22 19:50:15 +0530

    fixed cors

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use(
     origin: allowedOrigins,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization","token"],
   })
 );
 
